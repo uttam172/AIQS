05793441a08a1eec5296efbd269b9a691ecf21f8
uttam172 <uttamdobariya17@gmail.com>
2024-07-12 21:28:52 +0530

    bug fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -48,7 +48,7 @@ const Nav = () => {
 
                         <Link href="/profile">
                             <Image
-                                src={session.user.image ? (session.user.image) : ("/assets/images/logo.svg")}
+                                src={session?.user.image}
                                 width={37}
                                 height={37}
                                 className='rounded-full'
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -24,7 +24,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <div className="flex justify-between items-start gap-5">
                 <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
                     <Image
-                        src={post.creator.image ? (post.creator.image) : ("/assets/images/logo.svg")}
+                        src={post.creator?.image}
                         alt="userImage"
                         width={40}
                         height={40}
@@ -33,10 +33,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
                     <div className="flex flex-col">
                         <h3 className="font-satoshi font-semibold text-gray-900">
-                            {post.creator.username}
+                            {post.creator?.username}
                         </h3>
                         <p className="font-inter text-sm text-gray-500">
-                            {post.creator.email}
+                            {post.creator?.email}
                         </p>
                     </div>
                 </div>
@@ -63,7 +63,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             >
                 #{post.tag}
             </p>
-            {session?.user.id === post.creator._id && pathName === '/profile' && (
+            {session?.user.id === post.creator?._id && pathName === '/profile' && (
                 <div className="mt-5 flex-center gap-4 border-t  border-gray-300 pt-3">
                     <p
                         className="font-inter text-sm green_gradient cursor-pointer"
