a1a94cc2b5c95f53cba29e1c23c729d760c28ad6
uttam172 <uttamdobariya17@gmail.com>
2025-09-04 14:06:09 +0530

    bug fixed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -14,6 +14,7 @@ const handler = NextAuth({
 
     callbacks: {
         async session({ session }) {
+            await connectToDB()
 
             // store the user id from MongoDB to session
             const sessionUser = await User.findOne({ email: session.user.email })
